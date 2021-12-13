fe21ea2190a3502986ea6aaff608dc593a2e1bd0
Alex Hirsch <w4rh4wk@bluephoenix.at>
2021-12-13 19:20:54 +0100

    Add <leader>F to find all files

--- init.lua
+++ init.lua
@@ -101,6 +101,7 @@ packer.startup(function(use)
 			}
 			vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<cr>', {noremap = true})
 			vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>Telescope find_files<cr>', {noremap = true})
+			vim.api.nvim_set_keymap('n', '<leader>F', '<cmd>Telescope find_files hidden=true no_ignore=true<cr>', {noremap = true})
 			vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>Telescope live_grep<cr>', {noremap = true})
 		end
 	}
